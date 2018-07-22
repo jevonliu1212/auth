14c949919b80d3472ca9aaddd52c96a1a247d86f
liujianwei <245938612@qq.com>
2018-07-22 23:27:10 +0800

    update

--- src/main/java/com/v5/entity/User.java
+++ src/main/java/com/v5/entity/User.java
@@ -1,5 +1,81 @@
 package com.v5.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 用户实体类
+ *
+ * @author Jevon
+ * @time 2018年7月22日
+ * @copyright Jevon & Nate
+ */
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	/**
+	 * 用户名
+	 */
+	private String userName;
+
+	/**
+	 * 密码
+	 */
+	private String password;
+
+	/**
+	 * 邮箱
+	 */
+	private String email;
+
+	/**
+	 * 加盐
+	 */
+	private String salt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 
 }
